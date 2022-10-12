2fb5afb205a31c3c8fd5bedefcc39d317692cf94
Ji-Hong-PARK <113972972+Ji-Hong-PARK@users.noreply.github.com>
2022-10-12 17:27:34 +0900

    v0.5 10월12일 수업

---
+++ src/CH08/C00ClassInfo.java
@@ -0,0 +1,21 @@
+package CH08;
+
+public class C00ClassInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//1. 객체
+		
+		
+		//2.객체 지향
+		
+		
+		//3.클래스
+		
+		
+		//4.기본코드 해석
+	}
+
+}
