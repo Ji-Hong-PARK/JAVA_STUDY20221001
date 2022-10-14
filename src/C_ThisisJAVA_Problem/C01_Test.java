888c64c751ac2c6c8c0bed73014490203f1cc99d
Ji-Hong-PARK <113972972+Ji-Hong-PARK@users.noreply.github.com>
2022-10-14 11:12:53 +0900

    v0.6 10월14일 오전

---
+++ src/C_ThisisJAVA_Problem/C01_Test.java
@@ -0,0 +1,16 @@
+package C_ThisisJAVA_Problem;
+
+
+
+public class C01_Test {
+	public static void main(String[] args) {
+		
+		
+		// 이것이 자바다 6장 클래스 부분 예습 1번 문제 풀이
+		// 객체와 클래스에 대한 설명으로 틀린것은 무엇입니까?
+		
+		
+
+	}
+
+}
